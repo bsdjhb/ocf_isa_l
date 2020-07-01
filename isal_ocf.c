5fbe82720285b76f0a5529693d9619e1499b1b94
John Baldwin <jhb@FreeBSD.org>
2020-06-30 17:21:28 -0700

    Refactor to permit other algorithms besides GCM.

--- isal_ocf.c
+++ isal_ocf.c
@@ -54,25 +54,36 @@ struct isal_softc {
 	counter_u64_t sc_gcm_update_nt_calls;
 };
 
-struct isal_session {
+struct isal_gcm_session {
 	struct gcm_key_data key_data;
 
-	void (*aes_gcm_pre)(const void *, struct gcm_key_data *);
-	void (*aes_gcm_init)(const struct gcm_key_data *,
+	void (*pre)(const void *, struct gcm_key_data *);
+	void (*init)(const struct gcm_key_data *,
 	    struct gcm_context_data *, uint8_t *, uint8_t const *, uint64_t);
-	void (*aes_gcm_dec_update)(const struct gcm_key_data *,
+	void (*dec_update)(const struct gcm_key_data *,
 	    struct gcm_context_data *, uint8_t *, const uint8_t *, uint64_t);
-	void (*aes_gcm_dec_update_nt)(const struct gcm_key_data *,
+	void (*dec_update_nt)(const struct gcm_key_data *,
 	    struct gcm_context_data *, uint8_t *, const uint8_t *, uint64_t);
-	void (*aes_gcm_dec_finalize)(const struct gcm_key_data *,
+	void (*dec_finalize)(const struct gcm_key_data *,
 	    struct gcm_context_data *, uint8_t *, uint64_t);
-	void (*aes_gcm_enc_update)(const struct gcm_key_data *,
+	void (*enc_update)(const struct gcm_key_data *,
 	    struct gcm_context_data *, uint8_t *, const uint8_t *, uint64_t);
-	void (*aes_gcm_enc_update_nt)(const struct gcm_key_data *,
+	void (*enc_update_nt)(const struct gcm_key_data *,
 	    struct gcm_context_data *, uint8_t *, const uint8_t *, uint64_t);
-	void (*aes_gcm_enc_finalize)(const struct gcm_key_data *,
+	void (*enc_finalize)(const struct gcm_key_data *,
 	    struct gcm_context_data *, uint8_t *, uint64_t);
 };
+
+struct isal_session {
+	int	(*process)(struct isal_softc *, struct cryptop *,
+		    struct isal_session *);
+	union {
+		struct isal_gcm_session gcm;
+	};
+};
+
+static int isal_process_gcm(struct isal_softc *, struct cryptop *,
+    struct isal_session *);
 
 static MALLOC_DEFINE(M_ISAL, "isal", "ISA-L crypto");
 
@@ -124,16 +135,16 @@ isal_sysctls(device_t dev, struct isal_softc *sc)
 	    "AES-GCM decryption requests completed");
 	SYSCTL_ADD_COUNTER_U64(ctx, children, OID_AUTO, "gcm_update_bytes",
 	    CTLFLAG_RD, &sc->sc_gcm_update_bytes,
-	    "Bytes encrypted/decrypted by regular update");
+	    "Bytes encrypted/decrypted by regular AES-GCM update");
 	SYSCTL_ADD_COUNTER_U64(ctx, children, OID_AUTO, "gcm_update_nt_bytes",
 	    CTLFLAG_RD, &sc->sc_gcm_update_nt_bytes,
-	    "Bytes encrypted/decrypted by non-temporal update");
+	    "Bytes encrypted/decrypted by non-temporal AES-GCM update");
 	SYSCTL_ADD_COUNTER_U64(ctx, children, OID_AUTO, "gcm_update_calls",
 	    CTLFLAG_RD, &sc->sc_gcm_update_calls,
-	    "Calls to regular update functions");
+	    "Calls to regular AES-GCM update functions");
 	SYSCTL_ADD_COUNTER_U64(ctx, children, OID_AUTO, "gcm_update_nt_calls",
 	    CTLFLAG_RD, &sc->sc_gcm_update_nt_calls,
-	    "Calls to non-temporal update functions");
+	    "Calls to non-temporal AES-GCM update functions");
 }
 
 static int
@@ -212,6 +223,39 @@ isal_probesession(device_t dev, const struct crypto_session_params *csp)
 	return (CRYPTODEV_PROBE_ACCEL_SOFTWARE + 10);
 }
 
+static void
+isal_newsession_gcm(const struct crypto_session_params *csp,
+    struct isal_session *s)
+{
+	if (csp->csp_cipher_klen == 16) {
+		s->gcm.pre = aes_gcm_pre_128;
+		s->gcm.init = aes_gcm_init_128;
+		s->gcm.dec_update = aes_gcm_dec_128_update;
+		s->gcm.dec_update_nt = aes_gcm_dec_128_update_nt;
+		s->gcm.dec_finalize = aes_gcm_dec_128_finalize;
+		s->gcm.enc_update = aes_gcm_enc_128_update;
+		s->gcm.enc_update_nt = aes_gcm_enc_128_update_nt;
+		s->gcm.enc_finalize = aes_gcm_enc_128_finalize;
+	} else {
+		s->gcm.pre = aes_gcm_pre_256;
+		s->gcm.init = aes_gcm_init_256;
+		s->gcm.dec_update = aes_gcm_dec_256_update;
+		s->gcm.dec_update_nt = aes_gcm_dec_256_update_nt;
+		s->gcm.dec_finalize = aes_gcm_dec_256_finalize;
+		s->gcm.enc_update = aes_gcm_enc_256_update;
+		s->gcm.enc_update_nt = aes_gcm_enc_256_update_nt;
+		s->gcm.enc_finalize = aes_gcm_enc_256_finalize;
+	}
+
+	if (csp->csp_cipher_key != NULL) {
+		fpu_kern_enter(curthread, NULL, FPU_KERN_NOCTX);
+		s->gcm.pre(csp->csp_cipher_key, &s->gcm.key_data);
+		fpu_kern_leave(curthread, NULL);
+	}
+
+	s->process = isal_process_gcm;
+}
+
 static int
 isal_newsession(device_t dev, crypto_session_t cses,
     const struct crypto_session_params *csp)
@@ -220,47 +264,22 @@ isal_newsession(device_t dev, crypto_session_t cses,
 
 	s = crypto_get_driver_session(cses);
 
-	if (csp->csp_cipher_klen == 16) {
-		s->aes_gcm_pre = aes_gcm_pre_128;
-		s->aes_gcm_init = aes_gcm_init_128;
-		s->aes_gcm_dec_update = aes_gcm_dec_128_update;
-		s->aes_gcm_dec_update_nt = aes_gcm_dec_128_update_nt;
-		s->aes_gcm_dec_finalize = aes_gcm_dec_128_finalize;
-		s->aes_gcm_enc_update = aes_gcm_enc_128_update;
-		s->aes_gcm_enc_update_nt = aes_gcm_enc_128_update_nt;
-		s->aes_gcm_enc_finalize = aes_gcm_enc_128_finalize;
-	} else {
-		s->aes_gcm_pre = aes_gcm_pre_256;
-		s->aes_gcm_init = aes_gcm_init_256;
-		s->aes_gcm_dec_update = aes_gcm_dec_256_update;
-		s->aes_gcm_dec_update_nt = aes_gcm_dec_256_update_nt;
-		s->aes_gcm_dec_finalize = aes_gcm_dec_256_finalize;
-		s->aes_gcm_enc_update = aes_gcm_enc_256_update;
-		s->aes_gcm_enc_update_nt = aes_gcm_enc_256_update_nt;
-		s->aes_gcm_enc_finalize = aes_gcm_enc_256_finalize;
-	}
-
-	if (csp->csp_cipher_key != NULL) {
-		fpu_kern_enter(curthread, NULL, FPU_KERN_NOCTX);
-		s->aes_gcm_pre(csp->csp_cipher_key, &s->key_data);
-		fpu_kern_leave(curthread, NULL);
-	}
+	isal_newsession_gcm(csp, s);
 
 	return (0);
 }
 
 static int
-isal_process(device_t dev, struct cryptop *crp, int hint)
+isal_process_gcm(struct isal_softc *sc, struct cryptop *crp,
+    struct isal_session *s)
 {
 	struct gcm_context_data context_data;
 	struct crypto_buffer_cursor cc_in, cc_out;
-	struct isal_softc *sc;
-	struct isal_session *s;
 	void *aad;
 	uint8_t *in, *out;
-	void (*aes_gcm_update)(const struct gcm_key_data *,
+	void (*gcm_update)(const struct gcm_key_data *,
 	    struct gcm_context_data *, uint8_t *, const uint8_t *, uint64_t);
-	void (*aes_gcm_update_nt)(const struct gcm_key_data *,
+	void (*gcm_update_nt)(const struct gcm_key_data *,
 	    struct gcm_context_data *, uint8_t *, const uint8_t *, uint64_t);
 	uint8_t tag[16];
 	size_t inlen, outlen, resid, todo;
@@ -268,10 +287,8 @@ isal_process(device_t dev, struct cryptop *crp, int hint)
 	bool aad_allocated, fpu_entered;
 
 	aad_allocated = false;
-	if ((crp->crp_flags & CRYPTO_F_IV_SEPARATE) == 0) {
-		error = EINVAL;
-		goto out;
-	}
+	if ((crp->crp_flags & CRYPTO_F_IV_SEPARATE) == 0)
+		return (EINVAL);
 
 	/* Setup AAD. */
 	if (crp->crp_aad_length == 0)
@@ -283,18 +300,13 @@ isal_process(device_t dev, struct cryptop *crp, int hint)
 		    crp->crp_aad_length);
 		if (aad == NULL) {
 			aad = malloc(crp->crp_aad_length, M_ISAL, M_NOWAIT);
-			if (aad == NULL) {
-				error = ENOMEM;
-				goto out;
-			}
+			if (aad == NULL)
+				return (ENOMEM);
 			aad_allocated = true;
 			crypto_copydata(crp, crp->crp_aad_start,
 			    crp->crp_aad_length, aad);
 		}
 	}
-
-	sc = device_get_softc(dev);
-	s = crypto_get_driver_session(crp->crp_session);
 
 	if (is_fpu_kern_thread(0)) {
 		fpu_entered = false;
@@ -304,16 +316,16 @@ isal_process(device_t dev, struct cryptop *crp, int hint)
 	}
 
 	if (crp->crp_cipher_key != NULL)
-		s->aes_gcm_pre(crp->crp_cipher_key, &s->key_data);
-	s->aes_gcm_init(&s->key_data, &context_data, crp->crp_iv, aad,
+		s->gcm.pre(crp->crp_cipher_key, &s->gcm.key_data);
+	s->gcm.init(&s->gcm.key_data, &context_data, crp->crp_iv, aad,
 	    crp->crp_aad_length);
 
 	if (CRYPTO_OP_IS_ENCRYPT(crp->crp_op)) {
-		aes_gcm_update = s->aes_gcm_enc_update;
-		aes_gcm_update_nt = s->aes_gcm_enc_update_nt;
+		gcm_update = s->gcm.enc_update;
+		gcm_update_nt = s->gcm.enc_update_nt;
 	} else {
-		aes_gcm_update = s->aes_gcm_dec_update;
-		aes_gcm_update_nt = s->aes_gcm_dec_update_nt;
+		gcm_update = s->gcm.dec_update;
+		gcm_update_nt = s->gcm.dec_update_nt;
 	}
 
 	crypto_cursor_init(&cc_in, &crp->crp_buf);
@@ -329,8 +341,8 @@ isal_process(device_t dev, struct cryptop *crp, int hint)
 		outlen = crypto_cursor_seglen(&cc_out);
 
 		/*
-		 * Use aes_gcm_update_nt so long as both buffers are
-		 * 16 bytes aligned.
+		 * Use gcm_update_nt so long as both buffers are 16
+		 * bytes aligned.
 		 */
 		while (resid > 0 && inlen >= 16 && outlen >= 16 &&
 		    (uintptr_t)in % 16 == 0 && (uintptr_t)out % 16 == 0) {
@@ -338,7 +350,7 @@ isal_process(device_t dev, struct cryptop *crp, int hint)
 			if (todo > 16)
 				todo = rounddown2(todo, 16);
 
-			aes_gcm_update_nt(&s->key_data, &context_data, out, in,
+			gcm_update_nt(&s->gcm.key_data, &context_data, out, in,
 			    todo);
 
 			crypto_cursor_advance(&cc_in, todo);
@@ -363,13 +375,13 @@ isal_process(device_t dev, struct cryptop *crp, int hint)
 		}
 
 		/*
-		 * Fallback to aes_gcm_update once we hit any
-		 * unaligned data.
+		 * Fallback to gcm_update once we hit any unaligned
+		 * data.
 		 */
 		while (resid > 0) {
 			todo = MIN(MIN(inlen, outlen), resid);
 			
-			aes_gcm_update(&s->key_data, &context_data, out, in,
+			gcm_update(&s->gcm.key_data, &context_data, out, in,
 			    todo);
 
 			crypto_cursor_advance(&cc_in, todo);
@@ -398,7 +410,7 @@ isal_process(device_t dev, struct cryptop *crp, int hint)
 			if (todo > resid)
 				todo = resid;
 			in = crypto_cursor_segbase(&cc_in);
-			aes_gcm_update(&s->key_data, &context_data, in, in,
+			gcm_update(&s->gcm.key_data, &context_data, in, in,
 			    todo);
 			crypto_cursor_advance(&cc_in, todo);
 			resid -= todo;
@@ -410,14 +422,14 @@ isal_process(device_t dev, struct cryptop *crp, int hint)
 	error = 0;
 
 	if (CRYPTO_OP_IS_ENCRYPT(crp->crp_op)) {
-		s->aes_gcm_enc_finalize(&s->key_data, &context_data, tag,
+		s->gcm.enc_finalize(&s->gcm.key_data, &context_data, tag,
 		    sizeof(tag));
 		crypto_copyback(crp, crp->crp_digest_start, sizeof(tag), tag);
 		counter_u64_add(sc->sc_gcm_encrypt, 1);
 	} else {
 		uint8_t tag2[16];
 
-		s->aes_gcm_dec_finalize(&s->key_data, &context_data, tag,
+		s->gcm.dec_finalize(&s->gcm.key_data, &context_data, tag,
 		    sizeof(tag));
 		crypto_copydata(crp, crp->crp_digest_start, sizeof(tag), tag2);
 		if (timingsafe_bcmp(tag2, tag, 16) != 0)
@@ -431,9 +443,23 @@ isal_process(device_t dev, struct cryptop *crp, int hint)
 	explicit_bzero(&tag, sizeof(tag));
 	explicit_bzero(&context_data, sizeof(context_data));
 
-out:
 	if (aad_allocated)
 		free(aad, M_ISAL);
+	return (error);
+}
+
+static int
+isal_process(device_t dev, struct cryptop *crp, int hint)
+{
+	struct isal_softc *sc;
+	struct isal_session *s;
+	int error;
+
+	sc = device_get_softc(dev);
+	s = crypto_get_driver_session(crp->crp_session);
+
+	error = s->process(sc, crp, s);
+
 	crp->crp_etype = error;
 	crypto_done(crp);
 	return (0);
